d7d17d85b1640138f380a04770b8405d57d176ac
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-01-14 11:19:09 -0800

    Fetch places

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,15 +4,17 @@ This script file that contains the principal application
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
-from os import getenv
+from flask import Flask, make_response, jsonify, render_template
+from os import environ
 from flask_cors import CORS
 from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+# cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -37,7 +39,12 @@ Swagger(app)
 
 if __name__ == "__main__":
 
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
+    host = environ.get('HBNB_API_HOST', default='0.0.0.0')
+    port = environ.get('HBNB_API_PORT', default=5000)
 
-    app.run(host, int(port), threaded=True)
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+
+    app.run(host=host, port=port, threaded=True)
